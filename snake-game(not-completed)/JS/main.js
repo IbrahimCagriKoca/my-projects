29d4d38196036b891b90faff0cee74d6c708b3fb
IbrahimCagriKoca <cagrikoca12@gmail.com>
2019-11-20 03:07:44 +0300

    Add snake game folder

---
+++ snake-game(not-completed)/JS/main.js
@@ -0,0 +1,102 @@
+let canvas = document.getElementById('game');
+let context = canvas.getContext('2d');
+let grid = 16;
+let xgridsize = 800 / grid;
+let ygridsize = 800 / grid;
+let count = 0;
+
+let snake = {
+    x: 160,
+    y: 160,
+    dx: grid,
+    dy: 0,
+    cells: [],
+    maxCells: 4
+};
+let apple = {
+    x: 320,
+    y: 320
+};
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// game loop
+function loop() {
+    requestAnimationFrame(loop);
+    if (++count < 4) {
+        return;
+    }
+    count = 0;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    snake.x += snake.dx;
+    snake.y += snake.dy;
+    if (snake.x < 0) {
+        snake.x = canvas.width - grid;
+    } else if (snake.x >= canvas.width) {
+        snake.x = 0;
+    }
+
+    if (snake.y < 0) {
+        snake.y = canvas.height - grid;
+    } else if (snake.y >= canvas.height) {
+        snake.y = 0;
+    }
+
+    snake.cells.unshift({x: snake.x, y: snake.y});
+
+    if (snake.cells.length > snake.maxCells) {
+        snake.cells.pop();
+    }
+
+    context.fillStyle = 'red';
+    context.fillRect(apple.x, apple.y, grid - 1, grid - 1);
+
+    context.fillStyle = 'green';
+    snake.cells.forEach(function (cell, index) {
+
+        context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
+
+        if (cell.x === apple.x && cell.y === apple.y) {
+            snake.maxCells++;
+            apple.x = getRandomInt(0, xgridsize) * grid;
+            apple.y = getRandomInt(0, ygridsize) * grid;
+        }
+
+        for (let i = index + 1; i < snake.cells.length; i++) {
+
+            // snake occupies same space as a body part. reset game
+            if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+                snake.x = 160;
+                snake.y = 160;
+                snake.cells = [];
+                snake.maxCells = 4;
+                snake.dx = grid;
+                snake.dy = 0;
+                apple.x = getRandomInt(0, xgridsize) * grid;
+                apple.y = getRandomInt(0, ygridsize) * grid;
+            }
+        }
+    });
+}
+
+
+document.addEventListener('keydown', function (e) {
+
+    if (e.which === 37 && snake.dx === 0) {
+        snake.dx = -grid;
+        snake.dy = 0;
+    } else if (e.which === 38 && snake.dy === 0) {
+        snake.dy = -grid;
+        snake.dx = 0;
+    } else if (e.which === 39 && snake.dx === 0) {
+        snake.dx = grid;
+        snake.dy = 0;
+    } else if (e.which === 40 && snake.dy === 0) {
+        snake.dy = grid;
+        snake.dx = 0;
+    }
+});
+
+requestAnimationFrame(loop);
