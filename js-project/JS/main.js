691813f17a782f4768e531ae938b54d7fb3328d6
IbrahimCagriKoca <cagrikoca12@gmail.com>
2019-12-06 06:23:20 +0300

    Add js-challange update

--- js-project/JS/main.js
+++ js-project/JS/main.js
@@ -1,6 +1,7 @@
 function leftClose() {
     let middleDiv = document.getElementById("mid-div");
     let leftDiv = document.getElementById("left-div");
+
     if (middleDiv.style.width === "67%") {
         leftDiv.remove();
         middleDiv.style.width = "100%";
@@ -15,6 +16,7 @@ function leftClose() {
 function rightClose() {
     let middleDiv = document.getElementById("mid-div");
     let rightDiv = document.getElementById("right-div");
+
     if (middleDiv.style.width === "67%") {
         rightDiv.remove();
         middleDiv.style.width = "100%";
@@ -52,18 +54,17 @@ function expandLeft() {
             rightDiv.style.display = "block";
             leftDiv.style.width = "67%";
             leftArrow.style.display = "inline-block";
-            console.log(leftSpan.childElementCount);
             if (rightSpan.childElementCount > 3) {
                 rightSpan.removeChild(rightSpan.childNodes[6]);
             }
             rightDiv.style.width = "33%";
         }
     };
+
     if (leftDiv.style.width === "67%") {
         rightDiv.style.display = "none";
         leftDiv.style.width = "100%";
         rightArrow.style.display = "none";
-
     } else if (rightDiv.style.width === "67%") {
         rightDiv.style.display = "none";
         leftDiv.style.width = "100%";
@@ -76,7 +77,6 @@ function expandLeft() {
         middleDiv.style.display = "none";
         leftDiv.style.width = "67%";
         leftSpan.insertBefore(newArrow, leftSpan.childNodes[0]);
-
         if (newArrow.style.display === "none") {
             newArrow.classList.add("fas");
             newArrow.classList.add("fa-caret-left");
@@ -93,7 +93,6 @@ function expandRight() {
     let leftDiv = document.getElementById("left-div");
     let newArrow = document.createElement("BUTTON");
     let rightSpan = document.getElementById("right-span");
-    let leftArrow = document.getElementById("left-arrow");
     let rightArrow = document.getElementById("right-arrow");
     let leftSpan = document.getElementById("left-span");
     // rightSpan.appendChild(newArrow);
@@ -106,20 +105,17 @@ function expandRight() {
             rightDiv.style.width = "67%";
             leftDiv.style.width = "33%";
             leftDiv.style.display = "block";
-            console.log(newArrow.style.display);
             rightArrow.style.display = "inline-block";
-            console.log(leftSpan.childElementCount);
             if (leftSpan.childElementCount > 3) {
                 leftSpan.removeChild(leftSpan.childNodes[0]);
             }
         }
     };
+
     if (rightDiv.style.width === "67%") {
         leftDiv.style.display = "none";
         rightDiv.style.width = "100%";
         rightArrow.style.display = "none";
-
-
     } else if (leftDiv.style.width === "67%") {
         leftDiv.style.display = "none";
         rightDiv.style.width = "100%";
@@ -145,6 +141,7 @@ function expandBottom() {
     let bottomDiv = document.getElementById("bottom-div");
     let midText = document.getElementById("mid-span");
     let bottomArrow = document.getElementById("bottom-arrow");
+
     if (bottomDiv.style.height === "100%") {
         bottomDiv.style.height = "33%";
         midText.innerText = "Mid";
@@ -175,20 +172,21 @@ function moveLeft() {
     let middleDiv = document.getElementById("mid-div");
     let leftDiv = document.getElementById("left-div");
     let rightDiv = document.getElementById("right-div");
-    if (middleDiv.style.width === "33%" && leftArrow.style.display === "none") {
+
+    if (middleDiv.style.width === "34%" && leftArrow.style.display === "none") {
         leftText.innerText = "Left";
         leftArrow.style.display = "inline-block";
         leftClose.style.display = "inline-block";
         leftSquare.style.display = "inline-block";
         leftDiv.style.width = "33%";
-        middleDiv.style.width = "33%";
+        middleDiv.style.width = "34%";
     } else if (middleDiv.style.width === "64%" && leftArrow.style.display === "none") {
         leftText.innerText = "Left";
         leftArrow.style.display = "inline-block";
         leftClose.style.display = "inline-block";
         leftSquare.style.display = "inline-block";
         leftDiv.style.width = "33%";
-        middleDiv.style.width = "33%";
+        middleDiv.style.width = "34%";
     } else if (middleDiv.style.width === "64%" && rightDiv.style.width === "1.5%") {
         leftText.innerText = "";
         leftArrow.style.display = "none";
@@ -220,6 +218,7 @@ function moveBottom() {
     let bottomSquare = document.getElementById("bottom-square");
     let middleSpan = document.getElementById("mid-span");
     let bottomDiv = document.getElementById("bottom-div");
+
     if (bottomArrow.style.display === "none") {
         bottomText.innerText = "Bottom";
         bottomArrow.style.display = "inline-block";
@@ -245,20 +244,21 @@ function moveRight() {
     let middleDiv = document.getElementById("mid-div");
     let rightDiv = document.getElementById("right-div");
     let leftDiv = document.getElementById("left-div");
-    if (middleDiv.style.width === "33%" && rightArrow.style.display === "none") {
-        rightText.innerText = "Left";
-        rightArrow.style.display = "inline-block";
-        rightClose.style.display = "inline-block";
-        rightSquare.style.display = "inline-block";
-        rightDiv.style.width = "33%";
-        middleDiv.style.width = "33%";
-    } else if (middleDiv.style.width === "64%" && rightArrow.style.display === "none") {
+
+    if (middleDiv.style.width === "34%" && rightArrow.style.display === "none") {
         rightText.innerText = "Left";
         rightArrow.style.display = "inline-block";
         rightClose.style.display = "inline-block";
         rightSquare.style.display = "inline-block";
         rightDiv.style.width = "33%";
         middleDiv.style.width = "64%";
+    } else if (middleDiv.style.width === "64%" && rightArrow.style.display === "none") {
+        rightText.innerText = "Left";
+        rightArrow.style.display = "inline-block";
+        rightClose.style.display = "inline-block";
+        rightSquare.style.display = "inline-block";
+        rightDiv.style.width = "33%";
+        middleDiv.style.width = "34%";
     } else if (middleDiv.style.width === "64%" && leftDiv.style.width === "1.5%") {
         rightText.innerText = "";
         rightArrow.style.display = "none";
